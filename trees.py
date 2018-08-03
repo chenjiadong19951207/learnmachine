47c6907a42baf83137c99289f1e02cf90546df41
chenjiadong19951207 <c729208005@vip.qq.com>
2018-08-03 15:59:16 +0800

    决策树

--- trees.py
+++ trees.py
@@ -68,3 +68,20 @@ def marjorityCnt(classList):
         classCount[vote] += 1
         sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
         return sortedClassCount[0][0]
+
+def createTree(dataSet,labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) ==1:
+        return maityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestLabel][value] = createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
+    return myTree
