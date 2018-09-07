f6ae70311db743ba144e258e0a668e1b723483a3
chenjiadong19951207 <c729208005@vip.qq.com>
2018-09-07 13:35:38 +0800

    获取叶节点的数目和树的层数

--- treePlotter.py
+++ treePlotter.py
@@ -21,4 +21,27 @@ def createPlot():
     plt.show()
 
 
-createPlot()
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.key():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.key()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth
+    return maxDepth
+
+
+def re
